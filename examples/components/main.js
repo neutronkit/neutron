a9c0c2e81f9814c4a183b8c6da02b2f85c043005
arifcakiroglu <cakirogluarif@gmail.com>
2016-10-02 01:38:18 +0300

    electron example: components

---
+++ examples/components/main.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      height: 600,
+      width: 800,
+      frame: false
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
