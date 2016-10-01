9d82f742bafeff2b0af1d3f67bb5a6700656bb5f
arifcakiroglu <cakirogluarif@gmail.com>
2016-10-01 19:48:09 +0300

    add experimental script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+
+function onBlur() {
+	document.body.className = 'blurred';
+};
+function onFocus(){
+	document.body.className = 'focused';
+};
+
+
+window.onfocus = onFocus;
+window.onblur = onBlur;
